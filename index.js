49088b931cdbcd4f181ab02033e5471224a3dda3
myavidziqbal <sademark08@gmail.com>
2018-10-16 19:57:46 +0700

    Initial Commint

---
+++ index.js
@@ -0,0 +1,96 @@
+//Level 0 print variable----------------------------
+var numberA = 10;
+let numberB = 099;
+const numberC = 0.02;
+
+console.log(numberA)
+console.log("-------------------------------");
+//Level 1 Operator Math------------------------------
+
+let a = 2 + 2;
+
+let b = 3 - 2;
+
+let c = 3 * 3;
+
+let d = 4 / 2;
+
+let e = 7 % 2; // result:1
+
+let f = 2 - 3 * 2 / 3 + 1; // result:7
+
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log("------------------------");
+
+//Level 2 ------------------------------------
+
+let bool = true;
+let str = "I'm String";
+let numb = 1000;
+
+console.log(str);
+console.log(bool);
+console.log(numb);
+console.log("------------------------------");
+
+//Level 3 ---------------------------------
+ 
+// let arr = [10,12,42,23,50];
+let arr1 = ["a","b","c","d"];
+
+// let result = arr1.toString(arr1);
+// console.log(result);
+
+// let result = arr.join("_");
+// console.log(result);
+
+// let result = arr1.length();
+// console.log(result);
+
+// let result = arr1.includes("a");
+// console.log(result);
+
+// let result = arr1.push("e");
+// console.log(result);
+
+const list = ['a','b','c','d']
+for (let i = 0; i < list.length; i++) {
+    console.log(list[i])
+    // console.log(i)
+}
+
+// let result = arr1.pop("d");
+// console.log(result);
+console.log("------------------------------");
+
+//Level 4---------------------------------
+ const sentenceA = "Hello World";
+ const sentenceB = "Good Bye";
+
+ console.log(sentenceA);
+ console.log(sentenceB);
+
+ let paragA = "Yavidz";
+
+ console.log(paragA + " Iqbal")
+ console.log("------------------------------");
+
+ //Level 5----------------------------------
+
+ const sentenceC = sentenceA + " " + sentenceB;
+ 
+ console.log(sentenceC);
+
+ const sentenceD = `${sentenceA} ${sentenceB}`;
+
+ console.log(sentenceD);
+ console.log("------------------------------");
+
+ //Level 6---------------------------------
+ 
